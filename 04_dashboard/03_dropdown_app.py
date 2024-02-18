927cc9ea7522ed41e96e4926c88192df6bbe6055
P.Seferyniak <p.seferyniak@gmail.com>
2024-02-18 23:01:28 +0100

    update

--- 04_dashboard/03_dropdown_app.py
+++ 04_dashboard/03_dropdown_app.py
@@ -4,8 +4,7 @@
 """
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 
